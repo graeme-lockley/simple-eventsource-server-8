d604b7f9cd4a2c039e746f09197ddfe28729fb4f
Graeme Lockley <graemel@no9.co.za>
2019-03-10 07:34:19 +0200

    feature: create a default set of events on startup

--- jersey-api/src/test/java/za/co/no9/ses8/adaptors/Main.java
+++ jersey-api/src/test/java/za/co/no9/ses8/adaptors/Main.java
@@ -5,9 +5,11 @@ import org.glassfish.hk2.utilities.binding.AbstractBinder;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
 import za.co.no9.ses8.domain.ports.Repository;
+import za.co.no9.ses8.domain.ports.UnitOfWork;
 
 import java.io.IOException;
 import java.net.URI;
+import java.util.Random;
 
 
 public class Main {
@@ -32,11 +34,65 @@ public class Main {
         final Repository repository =
                 new TestRepositoryImpl();
 
+        insertRows(repository);
+
         final HttpServer server =
                 startServer(repository);
 
         System.out.println(String.format("Jersey app started with WADL available at %sapplication.wadl\nHit enter to stop it...", BASE_URI));
+
         System.in.read();
         server.shutdownNow();
+    }
+
+
+    private static final void insertRows(Repository repository) {
+        UnitOfWork unitOfWork =
+                repository.newUnitOfWork();
+
+        int lp = 0;
+        while (lp < 10000) {
+            switch (lp % 3) {
+                case 0:
+                    unitOfWork.saveEvent("CustomerAdded",
+                            "{name: \"" + randomString(1, 'A', 'Z') + randomString(8, 'a', 'z') + "\"," +
+                                    "customerID: " + lp + "}");
+                    break;
+
+                case 1:
+                    unitOfWork.saveEvent("AccountAdded",
+                            "{productID: \"CUR023\"," +
+                                    "number: \"" + randomString(10, '0', '9') + "\"," +
+                                    "customerID: " + (lp - 1) + "}");
+                    break;
+
+                case 2:
+                    unitOfWork.saveEvent("AccountAdded",
+                            "{productID: \"CUR027\"," +
+                                    "number: \"" + randomString(10, '0', '9') + "\"," +
+                                    "customerID: " + (lp - 2) + "}");
+                    break;
+            }
+
+            lp += 1;
+        }
+    }
+
+
+    private static String randomString(int targetStringLength, char leftLimit, char rightLimit) {
+        Random random =
+                new Random();
+
+        StringBuilder buffer =
+                new StringBuilder(targetStringLength);
+
+        for (int i = 0; i < targetStringLength; i++) {
+            int randomLimitedInt = leftLimit + (int)
+                    (random.nextFloat() * (rightLimit - leftLimit + 1));
+
+            buffer.append((char) randomLimitedInt);
+        }
+
+        return buffer.toString();
     }
 }
