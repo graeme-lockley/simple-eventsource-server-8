cb917a4b7fdc9d821e0f4874c6cd34bcfe221551
Graeme Lockley <graemel@no9.co.za>
2019-03-15 07:41:39 +0200

    fix: make the H2 database relative to the project's root path

--- h2-jersey-application/src/main/java/za/co/no9/ses8/application/Main.java
+++ h2-jersey-application/src/main/java/za/co/no9/ses8/application/Main.java
@@ -48,12 +48,10 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         final Jdbi jdbi =
-                Jdbi.create("jdbc:h2:/Users/graemelockley/Projects/simple-eventsource-server-8/main/target/stream", "sa", "");
+                Jdbi.create("jdbc:h2:./h2-jersey-application/target/stream", "sa", "");
 
         final Repository repository =
                 new H2(jdbi);
-
-//        insertRows(repository);
 
         final HttpServer server =
                 startServer(repository);
@@ -69,56 +67,5 @@ public class Main {
 
         System.in.read();
         server.shutdownNow();
-    }
-
-
-    private static final void insertRows(Repository repository) {
-        UnitOfWork unitOfWork =
-                repository.newUnitOfWork();
-
-        int lp = 0;
-        while (lp < 1000000) {
-            switch (lp % 3) {
-                case 0:
-                    unitOfWork.saveEvent("CustomerAdded",
-                            "{name: \"" + randomString(1, 'A', 'Z') + randomString(8, 'a', 'z') + "\"," +
-                                    "customerID: " + lp + "}");
-                    break;
-
-                case 1:
-                    unitOfWork.saveEvent("AccountAdded",
-                            "{productID: \"CUR023\"," +
-                                    "number: \"" + randomString(10, '0', '9') + "\"," +
-                                    "customerID: " + (lp - 1) + "}");
-                    break;
-
-                case 2:
-                    unitOfWork.saveEvent("AccountAdded",
-                            "{productID: \"CUR027\"," +
-                                    "number: \"" + randomString(10, '0', '9') + "\"," +
-                                    "customerID: " + (lp - 2) + "}");
-                    break;
-            }
-
-            lp += 1;
-        }
-    }
-
-
-    private static String randomString(int targetStringLength, char leftLimit, char rightLimit) {
-        Random random =
-                new Random();
-
-        StringBuilder buffer =
-                new StringBuilder(targetStringLength);
-
-        for (int i = 0; i < targetStringLength; i++) {
-            int randomLimitedInt = leftLimit + (int)
-                    (random.nextFloat() * (rightLimit - leftLimit + 1));
-
-            buffer.append((char) randomLimitedInt);
-        }
-
-        return buffer.toString();
     }
 }
