fef9b9b0d3a75dc799b4ff6897f5e3921e7a02fd
Graeme Lockley <graemel@no9.co.za>
2019-03-24 08:29:32 +0200

    refactor: create a standalone ui project which includes the swagger UI as well as the websocket interface

---
+++ ui-demo/src/main/java/za/co/no9/ses8/Main.java
@@ -0,0 +1,23 @@
+package za.co.no9.ses8;
+
+import io.javalin.Javalin;
+
+import java.io.IOException;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        final Javalin server =
+                Javalin
+                        .create()
+                        .port(8081)
+                        .enableStaticFiles("/")
+                        .disableStartupBanner()
+                        .start();
+
+        System.out.println("Server running  - hit enter to stop it...");
+
+        System.in.read();
+        server.stop();
+    }
+}
